dda1a2c905fb7841c15aea5b2b5ecf4ea851ec34
dinabouallegui <dinabouallegui0@gmail.com>
2023-03-12 22:00:09 +0100

    created templates and first view home

--- deliveryshop_main/deliveryshop_main/views.py
+++ deliveryshop_main/deliveryshop_main/views.py
@@ -3,7 +3,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hi this is the main page!")
-
-def about(request):
-    return HttpResponse("This is about section")
+    return render(request,'home.html')
