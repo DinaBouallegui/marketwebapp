738f6996e51509dacf9cf2cee1c66d1390068cb2
dinabouallegui <dinabouallegui0@gmail.com>
2023-03-13 18:47:54 +0100

    postgreSQL configuration is done

--- deliveryshop_main/deliveryshop_main/settings.py
+++ deliveryshop_main/deliveryshop_main/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'deliveryshop_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deliveryfood_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Ti-nspire2020',
+        'HOST': 'localhost',
     }
 }
 
