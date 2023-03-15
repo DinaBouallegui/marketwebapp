f462af2e2ff66a0dfe236adea266ce9d7867ed17
dinabouallegui <dinabouallegui0@gmail.com>
2023-03-15 22:57:00 +0100

    created signals to  userprofile

--- deliveryshop_main/useraccounts/models.py
+++ deliveryshop_main/useraccounts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager
 from django.db.models import OneToOneField
 
@@ -125,3 +127,19 @@ class UserProfile(models.Model):
     #__str__() is defined to return the email address of the user associated with that profile
     def __str__(self):
         return self.user.email
+
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender,instance,created,**kwargs):
+        print(created)
+        if created:
+            print('create the user profile')
+            UserProfile.objects.create(user=instance)
+        else: 
+            try: 
+                profile = UserProfile.objects.get(user=instance)
+                profile.save()
+            except:
+                #create the user profile if it doesnt exist
+                UserProfile.objects.create(user=instance)
+                print("profile wasn't existing but I created one")
+            print('user is updated')
