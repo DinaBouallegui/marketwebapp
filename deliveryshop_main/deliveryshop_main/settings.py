a276dc2db65556aa6127052d9bb66a4830ee8d02
dinabouallegui <dinabouallegui0@gmail.com>
2023-03-15 17:19:56 +0100

    configured media files

--- deliveryshop_main/deliveryshop_main/settings.py
+++ deliveryshop_main/deliveryshop_main/settings.py
@@ -86,6 +86,11 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'useraccounts.User'
 
+#configuring media files: 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
