f342f475ce9b24b15bfa32684c25db3fbcd84b80
dinabouallegui <dinabouallegui0@gmail.com>
2023-03-20 07:55:02 +0100

    adding useraccounts html page

--- deliveryshop_main/useraccounts/views.py
+++ deliveryshop_main/useraccounts/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def registerUser(request):
-    return HttpResponse('This is a user red form')
+    return render(request,'useraccounts/registerUser.html')
