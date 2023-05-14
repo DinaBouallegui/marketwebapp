ecdcff59963e240f154808fb6812bb0f21bed771
dinabouallegui <dinabouallegui0@gmail.com>
2023-05-14 14:55:25 +0200

    changed latitude and longitude readonl

--- deliveryshop_main/useraccounts/forms.py
+++ deliveryshop_main/useraccounts/forms.py
@@ -25,7 +25,12 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm): 
     profile_picture = forms.ImageField(widget = forms.FileInput(attrs={'class':'btn btn-info'}))
     cover_photo = forms.ImageField(widget = forms.FileInput(attrs={'class':'btn btn-info'}))
+    
+    latitude = forms.CharField(widget = forms.TextInput(attrs={'readonly':'readonly'}))
+    longitude = forms.CharField(widget = forms.TextInput(attrs={'readonly':'readonly'}))
+
     class Meta:
         model = UserProfile
         fields = ['profile_picture','cover_photo','address_line_1','address_line_2','country',
                   'state','city','pincode','longitude','latitude']
+    
