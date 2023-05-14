6217bc3917f502589b7f8ad7af1eb3f40f562791
dinabouallegui <dinabouallegui0@gmail.com>
2023-05-14 12:49:42 +0200

    added info of user to the form in restaurant profile and loaded the images

--- deliveryshop_main/vendor/views.py
+++ deliveryshop_main/vendor/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from useraccounts.forms import UserProfileForm
+from .forms import VendorForm
 
+from useraccounts.models import UserProfile
+from .models import Vendor
 # Create your views here.
 
 def vendorProfile(request):
-    return render(request, 'vendor/vendorProfile.html')
+    profile = get_object_or_404(UserProfile, user=request.user)
+    vendor = get_object_or_404(Vendor, user=request.user)
+
+    # by passing these instances inside the form, the form will load the existing content of this particular form
+
+    profile_form = UserProfileForm(instance = profile)
+    vendor_form = VendorForm(instance = vendor)
+    context ={
+        'profile_form': profile_form,
+        'vendor_form': vendor_form,
+        'profile': profile,
+        'vendor': vendor,
+    }
+    return render(request, 'vendor/vendorProfile.html',context)
