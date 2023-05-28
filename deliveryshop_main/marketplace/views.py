eb225e23b662bc7e1ff949088bc84dab469e8da2
dinabouallegui <dinabouallegui0@gmail.com>
2023-05-29 00:04:28 +0200

    added new cart model

--- deliveryshop_main/marketplace/views.py
+++ deliveryshop_main/marketplace/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
+from menu.models import Category, FoodItem
 
 from vendor.models import Vendor
+from django.db.models import Prefetch
 
 # Create your views here.
 
@@ -16,8 +18,19 @@ def marketplace(request):
 
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor, vendor_slug= vendor_slug)
+    # prefetch related will look for the data in a reverse manner
+    # because there is no food item foreign key in category model but want to get the food items that
+    # belong to one category
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        #make query inside the prefetch
+        Prefetch(
+        'fooditems',
+        queryset= FoodItem.objects.filter(is_available=True)
+        )
+    )
 
     context = {
         'vendor': vendor,
+        'categories' : categories,
     }
     return render(request,'marketplace/vendor_detail.html', context)
