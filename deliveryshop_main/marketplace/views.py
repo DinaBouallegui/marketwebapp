2d10c98b0aa6cf0873438c6dda52d0554e28f031
dinabouallegui <dinabouallegui0@gmail.com>
2023-05-28 21:03:03 +0200

    changes in restaurants listings

--- deliveryshop_main/marketplace/views.py
+++ deliveryshop_main/marketplace/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from vendor.models import Vendor
+
 # Create your views here.
 
 def marketplace(request):
-    return render(request,'marketplace/listings.html')
+    vendors = Vendor.objects.filter(is_approved=True,user__is_active=True)
+    count_vendor = vendors.count()
+    #counter here
+    context = {
+        'vendors' : vendors,
+        'count_vendor': count_vendor,
+    }
+    return render(request,'marketplace/listings.html',context)
