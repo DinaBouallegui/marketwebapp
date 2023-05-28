9bbc941eada965e464967209df03dc309fa7b334
dinabouallegui <dinabouallegui0@gmail.com>
2023-05-28 13:49:22 +0200

    added delete food crud

--- deliveryshop_main/deliveryshop_main/urls.py
+++ deliveryshop_main/deliveryshop_main/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('useraccounts/',include('useraccounts.urls')) 
+    path('',include('useraccounts.urls')) 
     #whenever someone comes to accounts, it will be forwarded to useraccounts app
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
