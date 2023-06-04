2c1ece46ab12279f5902f60d10c25a8fa872fbb1
dinabouallegui <dinabouallegui0@gmail.com>
2023-06-04 13:53:58 +0200

    move cart items to ordered food and create tabular inline

--- deliveryshop_main/orders/admin.py
+++ deliveryshop_main/orders/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from .models import OrderedFood,Payment,Order
 # Register your models here.
 
-admin.site.register(Order)
+class OrderedFoodInline(admin.TabularInline):
+    model = OrderedFood
+    readonly_fields = ('order','payment','user','fooditem','quantity','price','amount')
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display =  ['order_number','name','phone','email','total','payment_method','status','is_ordered']
+    inlines = [OrderedFoodInline]
+
+
+
+admin.site.register(Order,OrderAdmin)
 admin.site.register(OrderedFood)
 admin.site.register(Payment)
+
 
--- deliveryshop_main/orders/views.py
+++ deliveryshop_main/orders/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404, redirect, render
 from marketplace.context_processors import get_cart_amounts
 from marketplace.models import Cart
-from .models import Order
+from .models import Order, OrderedFood, Payment
 from .forms import OrderForm
 import simplejson as json
 from .utils import generate_order_number
@@ -60,4 +60,49 @@ def place_order(request):
 
 
 def payments(request):
+        #checking if the request is ajax or not 
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method == 'POST':
+        # storing the payment details in the payment model
+        # order number coming from ajax which is in request.post
+        order_number = request.POST.get('order_number')
+        transaction_id = request.POST.get('transaction_id')
+        payment_method = request.POST.get('payment_method')
+        status = request.POST.get('status')
+
+        order = Order.objects.get(user=request.user, order_number= order_number)
+        # create payment object
+        payment = Payment(
+            user = request.user,
+            transaction_id = transaction_id,
+            payment_method = payment_method,
+            amount = order.total,
+            status = status
+        )
+        payment.save()
+        # update the order model 
+        order.payment = payment
+        order.is_ordered = True
+        order.save()
+        # move the cart items to ordered food model
+        cart_items = Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            ordered_food = OrderedFood()
+            ordered_food.order = order
+            ordered_food.payment = payment
+            ordered_food.user = request.user
+            ordered_food.fooditem = item.fooditem
+            ordered_food.quantity = item.quantity
+            ordered_food.price = item.fooditem.price
+            ordered_food.amount = item.fooditem.price * item.quantity #this is the total amount 
+            ordered_food.save() 
+        return HttpResponse('save ordered food')
+
+        # send order confirmation email to the customer 
+
+        # send order received email to the vendor
+
+        # clear the cart if the pamyment is successfull 
+
+        # return back to ajax with the status success or failure
+
     return HttpResponse('Payments view')
